f2eef2115f72c8263817457f8381e3b98339a00c
Jeremy Phelps <jeremy@stockwell.ai>
2020-05-29 20:01:46 -0700

    Export the PROMISE symbol.

--- package.lisp
+++ package.lisp
@@ -1,4 +1,4 @@
 (cl:defpackage :cl-async-await
   (:use :closer-common-lisp :bordeaux-threads :simple-actors/ipc)
-  (:export :await :defun-async :lambda-async)
+  (:export :await :defun-async :lambda-async :promise)
   (:shadow assoc))
