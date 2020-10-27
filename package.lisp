4aa662d29d21389b531999a4b1b52bdc1a7ad6c7
Jeremy Phelps <jeremy@stockwell.ai>
2020-10-27 12:43:47 -0400

    Revert "Export the PROMISE symbol."
    This reverts commit f2eef2115f72c8263817457f8381e3b98339a00c.

--- package.lisp
+++ package.lisp
@@ -1,4 +1,4 @@
 (cl:defpackage :cl-async-await
   (:use :closer-common-lisp :bordeaux-threads :simple-actors/ipc)
-  (:export :await :defun-async :lambda-async :promise)
+  (:export :await :defun-async :lambda-async)
   (:shadow assoc))
